19d62fde58d71194925c4fdbef9f8897b72d9bb1
Mika Feiler <m@mikf.pl>
2021-04-26 11:12:20 +0200

    in use

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ system ? builtins.currentSystem, ... }:
+let
+  pkgs = import <nixpkgs> {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in
+rec {
+  base = with pkgs; [
+    firefox
+    file
+  ];
+  the_steam = pkgs.steam.override {
+        extraLibraries = pkgs: with pkgs;
+          [
+            libxkbcommon
+            mesa
+            #wayland
+            #(sndio.overrideAttrs (old: {
+            #  postFixup = old.postFixup + ''
+            #    ln -s $out/lib/libsndio.so $out/lib/libsndio.so.6.1
+            #  '';
+            #}))
+          ];
+      };
+  sztim = with pkgs; [
+    the_steam
+    the_steam.run
+#    steam-run
+#    steam-run-native
+  ];
+  cdda = pkgs.cataclysm-dda.withMods [
+    pkgs.cataclysm-dda.pkgs.tileset.UndeadPeople
+  ];
+}
