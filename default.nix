b7d3d5891efffdb8f4640596a567c7b607c19605
Mika Feiler <m@mikf.pl>
2021-07-22 19:39:48 +0200

    dodanie FHSowego vscode wraz z obszernym komentarzem

--- default.nix
+++ default.nix
@@ -33,4 +33,19 @@ rec {
   cdda = pkgs.cataclysm-dda.withMods [
     pkgs.cataclysm-dda.pkgs.tileset.UndeadPeople
   ];
+
+  # Poniższe to wersja pakietu do VS Code która wrapuje Code tak, by
+  # uruchamiał się w chroot'cie będącym compliant z Filesystem Hierarchy Standard,
+  # poprzez konstrukt NixOSowy buildFHSUserEnv. Reintrodukuje to
+  # katalogi takie jak /bin, /lib, czy /usr, niefunkcjonalne w NixOS,
+  # przez co prekompilowane binarki z rozszerzeń Code czy pakietów Pythona
+  # mogą działać bez konieczności ich indywidualnej "nixyfikacji".
+  code = pkgs.vscode-fhsWithPackages (ps: with ps; [
+    # rustup zlib  # needed for rust lang server extension
+    # (python39Full.withPackages (pp: with pp; [ pipenv ]))
+    pipenv
+    python39Full
+    python310
+  ]); # Kat, jeśli tu jesteś, to znaczy że niedługo możesz potrzebować
+      # bym nauczyła Cię Dockera — do VS Code Remote: Containers
 }
